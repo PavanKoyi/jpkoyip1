dec974982aa35849a7b2f981df533e90a2303289
PavanKoyi <koyi.pavankumar1215@gmail.com>
2021-04-08 21:20:59 -0400

    Updated the simple project with db credentials

--- project/simple/config.php
+++ project/simple/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'koyip1');
-define('DB_PASSWORD', 's4ewab');
-define('DB_NAME', 'koyip1_db');
+define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME', 'b45059cd74e131');
+define('DB_PASSWORD', '"76624f61');
+define('DB_NAME', 'heroku_422bace70db49be');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
